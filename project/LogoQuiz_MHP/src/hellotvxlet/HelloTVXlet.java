b7464e28c966c6df58dac3416246c0d9211e8257
lennVerberckt <lenn.verberckt@student.kdg.be>
2015-03-02 15:06:30 +0100

    wisselen van scermen
    Wisselen van schermen

--- project/LogoQuiz_MHP/src/hellotvxlet/HelloTVXlet.java
+++ project/LogoQuiz_MHP/src/hellotvxlet/HelloTVXlet.java
@@ -2,7 +2,6 @@ package hellotvxlet;
 
 import javax.tv.xlet.Xlet;
 import javax.tv.xlet.XletContext;
-import java.awt.*;
 import java.awt.event.*;
 import org.havi.ui.*;
 import org.dvb.ui.*;
@@ -17,7 +16,13 @@ public class HelloTVXlet implements Xlet, HActionListener {
     private HScene scene;
     private boolean debug = true;
     private HTextButton knop1;
+    private HTextButton knop2;
+    private HTextButton knop3;
     private HStaticText label1;
+    private HStaticText label2;
+    private HStaticText lblScore2;
+    public int Score = 0;
+    public Info spelregels;
     
     public void initXlet(XletContext context) {
         if(debug) System.out.println("Xlet initialiseren.");
@@ -29,22 +34,62 @@ public class HelloTVXlet implements Xlet, HActionListener {
     
         scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
         
-        label1 = new HStaticText("Welkom op de logoquiz");
+        label1 = new HStaticText("Welkom op ons spel");
         label1.setLocation(100, 50);
-        label1.setSize(400, 50);
+        label1.setSize(500, 50);
         label1.setBackground(new DVBColor(255,255,255,179));
         label1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        label1.setVisible(true);
+        
+        label2 = new HStaticText("typ de text correct over ");
+        label2.setLocation(200, 25);
+        label2.setSize(300, 25);
+        label2.setBackground(new DVBColor(255,255,255,179));
+        label2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        label2.setVisible(false);
+        
+        lblScore2 = new HStaticText(Integer.toString(Score));
+        lblScore2.setLocation(50, 75);
+        lblScore2.setSize(200, 25);
+        lblScore2.setBackground(new DVBColor(0,0,0,179));
+        lblScore2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        lblScore2.setVisible(false);
         
         knop1 = new HTextButton("Start");
-        knop1.setLocation(100,100);
-        knop1.setSize(250,50);
-        knop1.setBackground(new DVBColor(0,0,0,179));
+        knop1.setLocation(250,200);
+        knop1.setSize(200,50);
+        knop1.setBackground(new DVBColor(0,0,0,167));
         knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        knop1.setActionCommand("Het spel wordt gestart");
+        knop1.setActionCommand("start");
         knop1.addHActionListener(this);
+        knop1.setVisible(true);
+        
+        knop2 = new HTextButton("Stop");
+        knop2.setLocation(450,450);
+        knop2.setSize(200,50);
+        knop2.setBackground(new DVBColor(0,0,0,167));
+        knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        knop2.setActionCommand("stop");
+        knop2.addHActionListener(this);
+        knop2.setVisible(false);
+        
+        knop3 = new HTextButton("Enter");
+        knop3.setLocation(50,450);
+        knop3.setSize(200,50);
+        knop3.setBackground(new DVBColor(0,0,0,167));
+        knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        knop3.setActionCommand("enter");
+        knop3.addHActionListener(this);
+        knop3.setVisible(false);
+        
+        knop1.setFocusTraversal(null, knop2, null, null);
         
         scene.add(knop1);
+        scene.add(knop2);
+        scene.add(knop3);
         scene.add(label1);
+        scene.add(label2);
+        scene.add(lblScore2);
         
         knop1.requestFocus();
     }
@@ -66,5 +111,34 @@ public class HelloTVXlet implements Xlet, HActionListener {
     
     public void actionPerformed(ActionEvent e) {
         System.out.println(e.getActionCommand());
+        if (e.getActionCommand().equals("start"))
+        {
+            label1.setVisible(false);
+            knop1.setVisible(false);
+            label2.setVisible(true);
+            knop2.setVisible(true);
+            knop3.setVisible(true);
+            lblScore2.setVisible(true);
+            knop3.requestFocus();
+            knop2.setFocusTraversal(null, null, knop3, null);
+            knop3.setFocusTraversal(null, null, null, knop2);
+        }
+        else if (e.getActionCommand().equals("stop"))
+        {
+            label1.setVisible(true);
+            knop1.setVisible(true);
+            label2.setVisible(false);
+            knop2.setVisible(false);
+            knop3.setVisible(false);
+            lblScore2.setVisible(false);
+            knop1.requestFocus();
+            
+        }
+        
+        else if (e.getActionCommand().equals("enter"))
+        {
+            Score++;
+        }
     }
 }
+
